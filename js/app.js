b4fa32071a0c7d70b117db08535a12b861969a00
Nadim Rai <nadim_rai@yahoo.com>
2023-08-06 20:55:47 +0800

    li are dynamically added to ul

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let ul = document.querySelector('#navbar_ul');
+let list = document.querySelectorAll('.section_container');
+
+for(let i =0; i<list.length; i++){
+    let li = document.createElement('li');
+    let link = document.createElement('a');
+ 
+    link.href = `#${list[i].id}`;
+    link.innerHTML = list[i].dataset.nav;;
+    li.appendChild(link);
+    li.className = "navbar_li";
+    ul.appendChild(li);
+}
