7d32435262091e08e01d775f757983bd3258ac6a
Nadim Rai <nadim_rai@yahoo.com>
2023-08-18 21:39:21 +0800

    dynamically added li to ul in navigation section

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 let ul = document.querySelector('#navbar_ul');
 let list = document.querySelectorAll('.section_container');
+let active_section = document.querySelectorAll('.active_section');
 
+//Dynamically adding li to ul (Navigation)
 for(let i =0; i<list.length; i++){
     let li = document.createElement('li');
     let link = document.createElement('a');
  
     link.href = `#${list[i].id}`;
-    link.innerHTML = list[i].dataset.nav;;
+    link.innerHTML = list[i].dataset.nav;
     li.appendChild(link);
     li.className = "navbar_li";
     ul.appendChild(li);
 }
+
