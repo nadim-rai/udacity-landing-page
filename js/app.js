77e481fe9cbc3b2fe7de21bafb22cde7c40daa11
Nadim Rai <nadim_rai@yahoo.com>
2023-08-18 21:40:41 +0800

    clicking navigation item will scroll to corresponding section

--- js/app.js
+++ js/app.js
@@ -13,4 +13,15 @@ for(let i =0; i<list.length; i++){
     li.className = "navbar_li";
     ul.appendChild(li);
 }
+let nav_li = document.querySelectorAll('.navbar_li');
+
+//Scroll to respective section from navigation menu (Scroll to Anchor)
+nav_li.forEach(ele => {
+    ele.addEventListener('click', e =>{
+        e.preventDefault();
+        let ahref_id = ele.firstChild.getAttribute("href");
+        let section_id = document.querySelector(ahref_id);
+        section_id.scrollIntoView();
+    })
+})
 
